ed0a6c4486441988291a0dcd5cddadf7f8df8bff
ReaganG <rbgrunwald1997@gmail.com>
2021-03-16 14:18:27 -0500

    user can login through the nav bar

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function Login(){
+
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+
+    const loginUser = async () => {
+
+        const loggedInUser = await axios.post('/login', user);
+        console.log(loggedInUser);
+
+    }
+
     return(
         <div>
-            <input placeholder='username'/>
-            <input placeholder='password' type='password'/>
-            <button className='btn'>Login</button>
+            <input placeholder='username' onChange={(e) => setUser({...user, email: e.target.value})}/>
+            <input placeholder={user.email} type='password' onChange={(e) => setUser({...user, password: e.target.value})}/>
+            <button className='btn' onClick={loginUser}>Login</button>
         </div>
     )
 }
