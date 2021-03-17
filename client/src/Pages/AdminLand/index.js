afa3bd22a0d8971a2d0181c33b088a519fb11f81
ReaganG <rbgrunwald1997@gmail.com>
2021-03-17 12:39:56 -0500

    rout to admin landing page with champ choice

--- client/src/Pages/AdminLand/index.js
+++ client/src/Pages/AdminLand/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
+import Champ from '../../components/ChampChoice';
 
 function AdminLand() {
     return (
         <div>
             <h1>Admin Landing Page</h1>
             <h2>Insert some components here</h2>
+            <Champ/>
         </div>
     );
 };
