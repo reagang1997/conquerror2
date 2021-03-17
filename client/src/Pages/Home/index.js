285c4a01e2d7e9657cc31ed1f863b4e44be70881
Jose <jose42cordova@gmail.com>
2021-03-17 11:43:49 -0500

    champ landing page component

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.css';
+import Champ from "../../components/ChampChoice/ChampChoice";
 
 function Home() {
     return (
         <div class="jumbotron">
             <h1 class="display-4">Welcome to Conquerror!</h1>
             <img src="https://via.placeholder.com/400" alt="gif of making champ"/>
-
+            <Champ />
         </div>
     )
 }
