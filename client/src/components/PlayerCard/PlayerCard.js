ca4aa6ec674afa83a8ab93958fd7c9b1792c8a47
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-03-17 11:39:57 -0500

    card beginnings

--- client/src/components/PlayerCard/PlayerCard.js
+++ client/src/components/PlayerCard/PlayerCard.js
@@ -3,7 +3,28 @@ import "./style.css"
 
 const PlayerCard = () => {
     return (
-        <div>I am PlayerCard</div>
+        <div className="card">
+            <div className="card-body">
+                <div className="row">
+                    <div className="col">
+                        {/* This next bit will be appended in a list on the working version. This is mockup */}
+                        <div className="inputGroup">
+                            <input id="option1" name="option1" type="checkbox" />
+                            <label for="option1">Option One</label>
+                        </div>
+                    </div>
+                    <div className="col">
+
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Player's name" aria-label="Player's name" aria-describedby="button-addon2"/>
+                                <div className="input-group-append">
+                                    <button className="btn btn-outline-secondary" type="button" id="button-addon2">Add</button>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
