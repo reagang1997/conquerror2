d56d6fa6ffa91e45292db9cdb8f5bdc0a7441e1a
AngelCooper <74269067+AngelCooper@users.noreply.github.com>
2021-03-18 13:05:27 -0500

    changed team card

--- client/src/components/TeamCard/index.js
+++ client/src/components/TeamCard/index.js
@@ -3,58 +3,58 @@ import "./style.css"
 
 const TeamCard = () => {
     return (
-        <div class="teamCard" id="teamCard" style="width: 18rem;">
-        <div class="card-header">
-            Create Teams
+        <div class="teamCard" id="teamCard">
+            <div class="card-header">
+                Create Teams
+            </div>
+            <ul class="list-group list-group-flush" id="teamGroup">
+                <li class="list-group-item" id="team">Team One Name</li>
+
+                <input type="text" id="input-team"
+                    name="fname" placeholder="Team Name">
+                </input>
+
+                <li class="list-group-item" id="team">Team Two Name</li>
+
+                <input type="text" id="input-team"
+                    name="fname" placeholder="Team Name">
+                </input>
+
+                <li class="list-group-item" id="team">Team Three Name</li>
+
+                <input type="text" id="input-team"
+                    name="fname" placeholder="Team Name">
+                </input>
+
+
+                <li class="list-group-item" id="team">Team Four Name</li>
+
+                <input type="text" id="input-team"
+                    name="fname" placeholder="Team Name">
+                </input>
+
+
+                <li class="list-group-item" id="team">Team Five Name</li>
+
+                <input type="text" id="input-team"
+                    name="fname" placeholder="Team Name">
+                </input>
+
+
+                <li class="list-group-item" id="team">Team Six Name</li>
+
+                <input type="text" id="input-team"
+                    name="fname" placeholder="Team Name">
+                </input>
+
+
+            </ul>
+
+            <button class="button add" type="add" value="Add">Add Another Team</button>
+
+            <button class="button submit" type="submit" value="Search">Search</button>
+
         </div>
-        <ul class="list-group list-group-flush" id="teamGroup">
-            <li class="list-group-item" id="team">Team One Name</li>
-
-            <input type="text" id="input-team"
-                name="fname" placeholder="Team Name">
-            </input>
-
-            <li class="list-group-item" id="team">Team Two Name</li>
-
-            <input  type="text" id="input-team"
-                name="fname" placeholder="Team Name">
-            </input>
-            
-            <li class="list-group-item" id="team">Team Three Name</li>
-
-            <input  type="text" id="input-team"
-                name="fname" placeholder="Team Name">
-            </input>
-            
-
-            <li class="list-group-item" id="team">Team Four Name</li>
-
-            <input  type="text" id="input-team"
-                name="fname" placeholder="Team Name">
-            </input>
-           
-
-            <li class="list-group-item" id="team">Team Five Name</li>
-
-            <input  type="text" id="input-team"
-                name="fname" placeholder="Team Name">
-            </input>
-          
-
-            <li class="list-group-item" id="team">Team Six Name</li>
-
-            <input type="text" id="input-team"
-                name="fname" placeholder="Team Name">
-            </input>
-            
-
-        </ul>
-
-        <button class="button add" type="add" value="Add">Add Another Team</button>
-
-        <button class="button submit" type="submit" value="Search">Search</button>
-
-    </div>
     )
 };
 
