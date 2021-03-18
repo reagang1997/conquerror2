5f4d6e18d85fe70969e150a884a7db1588f2ae99
AngelCooper <74269067+AngelCooper@users.noreply.github.com>
2021-03-18 11:28:00 -0500

    Update TeamCard.js
    basic team card

--- client/src/components/TeamCard/TeamCard.js
+++ client/src/components/TeamCard/TeamCard.js
@@ -3,7 +3,57 @@ import "./style.css"
 
 const TeamCard = () => {
     return (
-        <div>I am TeamCard</div>
+        <div class="teamCard" id="teamCard" style="width: 18rem;">
+            <div class="card-header">
+                Create Teams
+            </div>
+            <ul class="list-group list-group-flush" id="teamGroup">
+                <li class="list-group-item" id="team">Team One Name</li>
+
+                <input class="rounderCorners padding-x-20" list="input-drop" type="text" id="input-search"
+                    name="fname" placeholder="Team Name">
+
+                </input>
+
+                <li class="list-group-item" id="team">Team Two Name</li>
+
+                <input class="rounderCorners padding-x-20" list="input-drop" type="text" id="input-search"
+                    name="fname" placeholder="Team Name">
+
+                </input>
+                <li class="list-group-item" id="team">Team Three Name</li>
+
+                <input class="rounderCorners padding-x-20" list="input-drop" type="text" id="input-search"
+                    name="fname" placeholder="Team Name">
+
+                </input>
+
+                <li class="list-group-item" id="team">Team Four Name</li>
+
+                <input class="rounderCorners padding-x-20" list="input-drop" type="text" id="input-search"
+                    name="fname" placeholder="Team Name">
+
+                </input>
+
+                <li class="list-group-item" id="team">Team Five Name</li>
+
+                <input class="rounderCorners padding-x-20" list="input-drop" type="text" id="input-search"
+                    name="fname" placeholder="Team Name">
+
+                </input>
+
+                <li class="list-group-item" id="team">Team Six Name</li>
+
+                <input class="rounderCorners padding-x-20" list="input-drop" type="text" id="input-search"
+                    name="fname" placeholder="Team Name">
+
+                </input>
+
+            </ul>
+
+            <button class="button submit" type="submit" value="Search">Search</button>
+
+        </div>
     )
 };
 
