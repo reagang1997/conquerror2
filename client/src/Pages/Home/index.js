45b1efbca18d4073fd2cb636c4eaef8954b35ca4
ReaganG <rbgrunwald1997@gmail.com>
2021-03-18 11:26:55 -0500

    working on moving search bar

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import { BrowserRouter as Router, Route, Redirect, useHistory } from "react-router-dom";
 import axios from 'axios';
 import AddChampBtn from '../../components/AddChampBtn';
+import Search from '../../components/Search'
 
 function Home() {
 
@@ -29,28 +30,34 @@ function Home() {
     }
 
     return (
-        <div className="row">
-            <div class="col-5 jumbotron">
-                <h1 class="display-4">Welcome to Conquerror!</h1>
-                <img src="https://via.placeholder.com/400" alt="gif of making champ" />
+        <div>
+            <div className='row'>
+                <div className='col-md-6'><Search /></div>
             </div>
+            <div className="row">
 
-            <div class="col-5 jumbotron">
-                <div class="row justify-content-center">
-                    <h1 class="display-4">Sign Up</h1>
+                <div class="col-5 jumbotron">
+                    <h1 class="display-4">Welcome to Conquerror!</h1>
+                    <img src="https://via.placeholder.com/400" alt="gif of making champ" />
                 </div>
-                <div class="row justify-content-center">
-                    <input placeholder='email' onChange={(e) => setUser({ ...user, email: e.target.value })}></input>
-                </div>
-                <div class="row justify-content-center">
-                    <input placeholder='username' onChange={(e) => setUser({ ...user, username: e.target.value })}></input>
-                </div>
-                <div class="row justify-content-center">
-                    <input placeholder='password' type='password' onChange={(e) => setUser({ ...user, password: e.target.value })}></input>
-                </div>
-                <div class="row justify-content-center">
-                    <button className="btn" onClick={createUser}>Login</button>
-                    {user.stats === 200 ? <Redirect to='/adminHome' /> : console.log('not logged in')}
+
+                <div class="col-5 jumbotron">
+                    <div class="row justify-content-center">
+                        <h1 class="display-4">Sign Up</h1>
+                    </div>
+                    <div class="row justify-content-center">
+                        <input placeholder='email' onChange={(e) => setUser({ ...user, email: e.target.value })}></input>
+                    </div>
+                    <div class="row justify-content-center">
+                        <input placeholder='username' onChange={(e) => setUser({ ...user, username: e.target.value })}></input>
+                    </div>
+                    <div class="row justify-content-center">
+                        <input placeholder='password' type='password' onChange={(e) => setUser({ ...user, password: e.target.value })}></input>
+                    </div>
+                    <div class="row justify-content-center">
+                        <button className="btn" onClick={createUser}>Login</button>
+                        {user.stats === 200 ? <Redirect to='/adminHome' /> : console.log('not logged in')}
+                    </div>
                 </div>
             </div>
         </div>
