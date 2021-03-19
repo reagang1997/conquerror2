2bd7d3917db893468660e0aa1c1a1a967ba2ee00
ReaganG <rbgrunwald1997@gmail.com>
2021-03-19 10:13:42 -0500

    link to create champ page

--- client/src/components/AddChampBtn/index.js
+++ client/src/components/AddChampBtn/index.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Redirect, useHistory } from "react-router-dom";
+
 import './style.css';
 
 
 const NewChampBtn = () => {
 
-
+    const history = useHistory();
 
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-4 newChamp">
-                    <button className="addChampBtn">Add New Champ</button>
+                    <button className="addChampBtn" onClick={(e) => {
+                        history.push('/createChampionship');
+                    }}>Add New Champ</button>
                 </div>
             </div>
         </div>
