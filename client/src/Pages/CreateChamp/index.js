060444241e3a02b4eed2b593a675d9e3b0878c08
AngelCooper <74269067+AngelCooper@users.noreply.github.com>
2021-03-22 12:01:48 -0500

    added create champ form onto page header

--- client/src/Pages/CreateChamp/index.js
+++ client/src/Pages/CreateChamp/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import './style.css';
-import DemoCard from "../../components/NewDemoCardlol";
+import { InputGroup, FormControl, Button} from 'react-bootstrap';
 import PlayerCard from '../../components/PlayerCard';
 import StatCard from '../../components/StatCard';
 import TeamCard from '../../components/TeamCard';
 import SaveBtn from '../../components/SaveBtn';
 
+
 function CreateChamp() {
     return (
         <div className="container">
             <br />
-            <h1 className="title shadow">Create Champ Page</h1>
-            <StatCard className="shadow"/>
+
+            <div>
+                <h1 className="title shadow">Create Champ Page
+                 <InputGroup className="mb-3">
+                        <FormControl
+                            placeholder="Create Champ League"
+                            aria-label="Create Champ League"
+                            aria-describedby="basic-addon2"
+                        />
+                    </InputGroup>
+
+                    <Button variant="dark">Create</Button>
+                </h1>
+            </div>
+
+
+
+
+
+            <StatCard className="shadow" />
             <TeamCard />
             <PlayerCard />
             <SaveBtn />
