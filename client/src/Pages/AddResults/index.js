79eb8f659a5c29643ae46ea4e2d79636918e9325
Jose <jose42cordova@gmail.com>
2021-03-23 10:23:34 -0500

    results card attempt

--- client/src/Pages/AddResults/index.js
+++ client/src/Pages/AddResults/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ResultsCard from '../../components/ResultsCard';
+import NewResults from '../../components/NewResultCardAttemptlol';
 import SaveBtn from '../../components/SaveBtn';
+import PrevNxt from '../../components/NextPrevSave';
 import './style.css';
 
 function AddResults() {
     return (
         <div>
-            <ResultsCard />
-            <SaveBtn />
+            <br />
+            <div className="title shadow">
+                <h1 id={'create-champ'}>Update Results</h1>
+            </div>
+            <NewResults />
+            {/* <ResultsCard /> */}
+            <br />
+            <PrevNxt />
+            {/* <SaveBtn /> */}
         </div>
     );
 };
