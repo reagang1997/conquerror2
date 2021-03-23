9350b8cf79d99177e3c143bda6715bad89402bdc
ReaganG <rbgrunwald1997@gmail.com>
2021-03-23 15:59:36 -0500

    round robin on prev / next buttons

--- client/src/components/NextPrevSave/index.js
+++ client/src/components/NextPrevSave/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { Container, Button, Row, Col } from 'react-bootstrap';
 import './style.css';
 
-const PrevNextBtns = ({index, setIndex}) => {
+const PrevNextBtns = ({ index, setIndex, players }) => {
     return (
         <Container>
             <Row>
-                <Col><Button variant='dark' block onClick={(e) => setIndex(index - 1)}>Prev...</Button></Col>
+                <Col><Button variant='dark' block onClick={(e) => {
+                    if (index === 0) {
+                        setIndex(players.length - 1)
+                    }
+                    else{
+                        setIndex(index - 1)
+                    }
+                }}>Prev...</Button></Col>
                 <Col><Button variant='dark' block>Save</Button></Col>
-                <Col><Button variant='dark' block onClick={(e) => setIndex(index + 1)}>...Next</Button></Col>
+                <Col><Button variant='dark' block onClick={(e) => {
+                    if (index === players.length - 1) {
+                        setIndex(0)
+                    }
+                    else{
+                        setIndex(index + 1)
+                    }
+                }}>...Next</Button></Col>
             </Row>
         </Container>
     );
