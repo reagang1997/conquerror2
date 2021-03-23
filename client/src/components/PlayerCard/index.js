494a81e173f3f13beea2d0bea23d1cdbb21cc4f6
Jose <jose42cordova@gmail.com>
2021-03-23 13:15:58 -0500

    player card dropdown change

--- client/src/components/PlayerCard/index.js
+++ client/src/components/PlayerCard/index.js
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Card, ListGroup, Button, Form, Dropdown, DropdownButton, InputGroup, FormControl } from 'react-bootstrap';
 import InputStat from '../InputStat';
 import './style.css';
 
-const PlayerCard = ({teams, players, setPlayers}) => {
+const PlayerCard = ({ teams, players, setPlayers }) => {
 
     const [tmpPlayer, setTmpPlayer] = useState({
         name: ""
@@ -18,7 +18,7 @@ const PlayerCard = ({teams, players, setPlayers}) => {
 
                     <h1>Players</h1>
                     <br />
-                    {players ? players.map(player => <InputStat name={player.name}/>) : <div></div> }
+                    {players ? players.map(player => <InputStat name={player.name} />) : <div></div>}
 
                 </Col>
 
@@ -30,30 +30,40 @@ const PlayerCard = ({teams, players, setPlayers}) => {
                     <br />
                     <InputGroup>
                         <FormControl
-                            placeholder="new team name"
-                            aria-label="new team name"
+                            placeholder="new player name"
+                            aria-label="new player name"
                             aria-describedby="basic-addon2"
                             value={tmpPlayer.name}
                             onChange={e => setTmpPlayer({ name: e.target.value })}
                         />
 
-                        <DropdownButton
+                        {/* <DropdownButton
                             as={InputGroup.Append}
                             variant="outline-dark"
                             title="Dropdown"
                             id="input-group-dropdown-2"
                         >
                             {teams ? teams.map(team => <Dropdown.Item href="#">{team.name}</Dropdown.Item>) : console.log('no teams')}
-                        </DropdownButton>
+                        </DropdownButton> */}
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Control as="select">
+                                <option>Team 1</option>
+                                <option>Team 2</option>
+                                <option>Team 3</option>
+                                <option>Team 4</option>
+                                <option>Team 5</option>
+                            </Form.Control>
+                        </Form.Group>
+
                     </InputGroup>
                     <br />
-                   
+
                     <Button variant="dark" block className="right test"
-                    onClick={e => {
-                        e.preventDefault();
-                        setPlayers([...players, tmpPlayer]);
-                        setTmpPlayer({name: ""});
-                    }}>Add Player</Button>
+                        onClick={e => {
+                            e.preventDefault();
+                            setPlayers([...players, tmpPlayer]);
+                            setTmpPlayer({ name: "" });
+                        }}>Add Player</Button>
 
                 </Col>
             </Row>
