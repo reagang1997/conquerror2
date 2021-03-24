33f2e1061cb06c04c38121c97e6d0c717d912c47
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-03-24 12:04:46 -0500

    name change

--- client/src/components/NavTabs/index.js
+++ client/src/components/NavTabs/index.js
@@ -9,7 +9,7 @@ function NavTabs({ isLoggedIn, setLoggedIn}) {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
-            <a className="navbar-brand" href="/">StaTrack</a>
+            <a className="navbar-brand" href="/">Championship Tracker</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
