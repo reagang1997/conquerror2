4c9974814205f6f2e9639d3083fae927085342a9
ReaganG <rbgrunwald1997@gmail.com>
2021-03-23 21:44:24 -0500

    merge main 2

--- client/src/Pages/SearchResults/index.js
+++ client/src/Pages/SearchResults/index.js
@@ -5,11 +5,6 @@ import './style.css';
 
 function SearchResults({search, setSearch}) {
     return (
-<<<<<<< HEAD
-        <div>
-            <h1>{search}</h1>
-            <h2>Insert some components here</h2>
-=======
         <div className='container'>
             <br />
             <div className='center' >
@@ -20,7 +15,6 @@ function SearchResults({search, setSearch}) {
             </div>
             <br />
             <Champ/>
->>>>>>> 3f4d0cf230afcf3d29a6c524d9839f80ecf1a749
         </div>
     );
 };
